4ec7e0983e6f6911066c8068cc5155c1ccda5594
JuanGarcia162 <juadgarca@misena.edu.co>
2022-02-08 12:20:53 -0500

    Proyecto terminado

---
+++ src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+
+  weather: any;
+
+  constructor(private weatherService: WeatherService){
+
+  }
+
+  ngOnInit(){
+    
+  }
+
+  getWeather(cityName: string, countryCode: string){
+    this.weatherService.getWeather(cityName, countryCode)
+    .subscribe(
+      res => {
+        console.log(res);
+        this.weather = res
+      },
+      err => console.log(err)
+    )
+  }
+
+  submitLocation(cityName: HTMLInputElement, countryCode:HTMLInputElement){
+    if (cityName.value && countryCode.value) {
+      this.getWeather(cityName.value, countryCode.value)
+      cityName.value = '';
+      countryCode.value = '';
+      
+    }else {
+      alert("Por favor, inserte los datos")
+    }
+      cityName.focus();
+
+      return false
+    
+  }
+
+}
